a37a4d711de38e97f358dc793d4e56ab57d163b5
student <student@p.lodz.pl>
2025-06-02 13:44:12 +0200

    added more methods v2

--- KlasyObiekty1.cpp
+++ KlasyObiekty1.cpp
@@ -1,4 +1,4 @@
-﻿// KlasyObiekty1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+﻿// KlasyObiekty3.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
 //
 
 #include <iostream>
@@ -8,25 +8,19 @@ using namespace std;
 int main()
 {
   //Point p1, p2, p3;
-  Point p1;
+  Point p1("P1", 0, 0);
   Point p2("P2", 30, 40);
-  Point p3 = Point("P3");
-  Point p4 = { "P4", 30, 40 };
-  Point p5 {};
-
-  p1.setName("P1");
-  p1.setXY(10, 20);
-  p2.setXY(30, 40);
 
   p1.show();
   p2.show();
-  p3.show();
-  p4.show();
-  p5.show();
 
-  p2 = p1;
-  p2.show();
+  cout << "Odleglosc pomiedzy punktami wynosi " << p1.distance(p2) << endl;
+  const Point& p = p1.distant(p2);
+  cout << "Punkt bardziej oddalonny od poczatku ukladu wspolrzednych to: " << endl;
+  p.show();
+
 }
+
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
 // Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
 
